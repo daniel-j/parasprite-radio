dcd50fbc124f27a0f91f48e464aef8393507887f
daniel-j <daniel.j.hede@gmail.com>
2015-11-25 10:53:11 +0100

    moore

--- liq/scripts/metadata.js
+++ liq/scripts/metadata.js
@@ -10,14 +10,39 @@ var ext = path.extname(process.argv[2]).substr(1)
 var name = path.basename(process.argv[2])
 
 mediainfo(process.argv[2]).then(function (res) {
+	//delete res[0].cover_data
+	//console.error(res[0])
+
 	if (res[0].track_name) {
 		output.title = res[0].track_name
 	}
 	if (res[0].performer) {
 		output.artist = res[0].performer
 	}
+	if (res[0].album) {
+		output.album = res[0].album
+	}
+	if (res[0].album_performer) {
+		output.albumartist = res[0].album_performer
+	}
+	if (res[0].recorded_date) {
+		output.year = parseInt(res[0].recorded_date, 10)
+	}
+	if (res[0].genre) {
+		output.genre = res[0].genre
+	}
+	if (res[0].duration) {
+		var d = res[0].duration.match(/^(?:(\d*)h)?\s?(?:(\d*)mn)?\s?(?:(\d*)s)?$/)
+		if (d) {
+			var duration = (d[1]||0)*60*60 + (d[2]||0)*60 + (d[3]||0)*1
+			output.time = duration
+		}
+	}
 	console.log(JSON.stringify(output))
+
 }).catch(function (err) {
+	console.error('Mediainfo '+err.error)
+
 	if (xmp_ext.indexOf(ext.toLowerCase()) !== -1) {
 		output.title = name
 
