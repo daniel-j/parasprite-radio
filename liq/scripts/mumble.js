642663bc63563f6caffce72d9c36d3ce7bdbfcbd
daniel-j <daniel.j.hede@gmail.com>
2016-01-19 21:44:48 +0100

    hopefully fixes it

--- liq/scripts/mumble.js
+++ liq/scripts/mumble.js
@@ -10,15 +10,30 @@ var options = {
 	// key: fs.readFileSync( 'mumble-key.pem' ),
 	// cert: fs.readFileSync( 'mumble-cert.pem' )
 }
+var botname = config.mumble.botname
 
 mumble.connect(config.mumble.uri, options, function ( error, connection ) {
 	if (error) {
 		throw new Error( error )
 	}
 
-	connection.authenticate(config.mumble.botname)
-	connection.on('initialized', function () {
-		process.stdin.pipe(connection.inputStream({gain: 0.6, channels: 2}))
+	var inputStream = connection.inputStream({gain: 0.5, channels: 1})
+	inputStream.on('error', function (e) {
+		console.log('ERROR!!', e)
 	})
-})
 
+	connection.authenticate(botname)
+	connection.on('initialized', function () {
+		process.stdin.pipe(inputStream)
+	})
+
+	connection.on('user-disconnect', function (user) {
+		if (user.name === botname) {
+			inputStream.close()
+			connection.disconnect()
+			process.stdin.end()
+			process.exit()
+		}
+	})
+
+})
