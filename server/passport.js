ed98687929d18be1102e8814efae34a2a9fc4dc7
daniel-j <daniel.j.hede@gmail.com>
2016-10-27 16:06:39 +0200

    lint. check for enabled strategies

--- server/passport.js
+++ server/passport.js
@@ -6,63 +6,65 @@ import { fetchJSON } from '../scripts/fetcher'
 
 export default function (passport) {
 
-	// used to serialize the user for the session
-	passport.serializeUser(function (user, done) {
-		// console.log("SERIALIZE", user.id)
-		done(null, user.id)
-	})
+  // used to serialize the user for the session
+  passport.serializeUser(function (user, done) {
+    // console.log("SERIALIZE", user.id)
+    done(null, user.id)
+  })
 
-	// used to deserialize the user
-	passport.deserializeUser(function (id, done) {
-		// console.log "UNSERIALIZE", id
-		User.findById(id, (err, user) => done(err, user))
-	})
+  // used to deserialize the user
+  passport.deserializeUser(function (id, done) {
+    // console.log "UNSERIALIZE", id
+    User.findById(id, (err, user) => done(err, user))
+  })
 
+  if (config.passport.poniverse && config.passport.poniverse.clientID) {
+    const poniverse = new OAuth2Strategy(config.passport.poniverse, function (accessToken, refreshToken, profile, done) {
+      let provider = 'poniverse'
+      fetchJSON('https://api.poniverse.net/v1/users/me?access_token=' + accessToken, null, function (err, data) {
+        if (err) {
+          done(err, null)
+          return
+        }
 
-	const poniverse = new OAuth2Strategy(config.passport.poniverse, function (accessToken, refreshToken, profile, done) {
-		let provider = 'poniverse'
-		fetchJSON('https://api.poniverse.net/v1/users/me?access_token='+accessToken, null, function (err, data) {
-			if (err) {
-				done(err, null)
-				return
-			}
+        let userInfo = {
+          provider: provider,
+          accessToken: accessToken,
+          refreshToken: refreshToken,
+          uid: data.id,
 
-			let userInfo = {
-				provider: provider,
-				accessToken: accessToken,
-				refreshToken: refreshToken,
-				uid: data.id,
+          username: data.username,
+          displayName: data.display_name,
+          email: data.email,
+          level: 0,
+          avatarUrl: ''
+        }
 
-				username: data.username,
-				displayName: data.display_name,
-				email: data.email,
-				level: 0,
-				avatarUrl: ''
-			}
+        User.handleAuth(userInfo, done)
+      })
+    })
 
-			User.handleAuth(userInfo, done)
-		})
-	})
+    poniverse.name = 'poniverse' // replace 'oauth2'
+    passport.use(poniverse)
+  }
 
-	poniverse.name = 'poniverse' // replace 'oauth2'
-	passport.use(poniverse)
+  if (config.passport.twitter && config.passport.twitter.consumerKey) {
+    passport.use(new TwitterStrategy(config.passport.twitter, function (accessToken, refreshToken, profile, done) {
+      let provider = 'twitter'
 
-	passport.use(new TwitterStrategy(config.passport.twitter, function (accessToken, refreshToken, profile, done) {
-		let provider = 'twitter'
+      let userInfo = {
+        provider: provider,
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+        uid: profile.id,
 
-		let userInfo = {
-			provider: provider,
-			accessToken: accessToken,
-			refreshToken: refreshToken,
-			uid: profile.id,
+        username: profile.username,
+        displayName: profile.displayName,
+        level: 0,
+        avatarUrl: profile.photos[0].value
+      }
 
-			username: profile.username,
-			displayName: profile.displayName,
-			level: 0,
-			avatarUrl: profile.photos[0].value
-		}
-
-		process.nextTick(() => User.handleAuth(userInfo, done))
-	}))
-
+      process.nextTick(() => User.handleAuth(userInfo, done))
+    }))
+  }
 }
