350d6b6be7d4cfcc28e55e97a4531d68ed912e1a
daniel-j <daniel.j.hede@gmail.com>
2016-12-08 19:17:15 +0100

    Make node server reloadable without downtime, added readline for command input.

--- server/cluster.js
+++ server/cluster.js
@@ -9,23 +9,97 @@ import colors from 'colors/safe'
 // const numCores = require('os').cpus().length
 
 let args
+let rl
+const workers = []
 
-async function initialize () {
-  const knex = require('knex')(require('../knexfile'))
-  args.verbose && console.log('Initializing database...')
-  await knex.migrate.latest()
-  args.verbose && console.log('Database initialized')
-  await knex.destroy()
+function startReadline () {
+  const readline = require('readline')
+  rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  rl.setPrompt('> ')
+  rl.on('line', (line) => {
+    if (line === 'reload') {
+      workers.forEach((w, i) => {
+        spawnWorker(w)
+      })
+      workers.length = 0
+    }
+    rl.prompt()
+  })
+  rl.on('close', () => {
+    rl.output.write('\n')
+    process.exit(0)
+  })
+  rl.prompt()
+
+  const realConsoleLog = console.log
+  console.log = function () {
+    rl.output.write('\x1b[2K\r')
+    realConsoleLog.apply(this, arguments)
+    rl.prompt(true)
+  }
+  const realConsoleWarn = console.warn
+  console.log = function () {
+    rl.output.write('\x1b[2K\r')
+    realConsoleWarn.apply(this, arguments)
+    rl.prompt(true)
+  }
+  const realConsoleError = console.error
+  console.log = function () {
+    rl.output.write('\x1b[2K\r')
+    realConsoleError.apply(this, arguments)
+    rl.prompt(true)
+  }
 }
 
-function spawnWorker () {
+async function initialize () {
+  try {
+    const knex = require('knex')(require('../knexfile'))
+    args.verbose && console.log('Initializing database...')
+    await knex.migrate.latest()
+    args.verbose && console.log('Database initialized')
+    await knex.destroy()
+  } catch (err) {
+    console.error(colors.red('Cluster: Database error:'), err)
+  }
+  startReadline()
+}
+
+function spawnWorker (oldWorker) {
   const w = cluster.fork()
-  args.verbose && console.log(colors.green('Cluster: Started worker process ' + w.process.pid))
+  w.process.stdout.on('data', (data) => {
+    rl.output.write('\x1b[2K\r')
+    process.stdout.write(data)
+    rl.prompt(true)
+  })
+  w.process.stderr.on('data', (data) => {
+    rl.output.write('\x1b[2K\r')
+    process.stderr.write(data)
+    rl.prompt(true)
+  })
+  args.verbose && console.log(colors.cyan('Cluster: Starting worker process ' + w.process.pid + '...'))
+
+  w.on('message', (message) => {
+    if (message === 'started') {
+      workers.push(w)
+      args.verbose && console.log(colors.green('Cluster: Started worker process ' + w.process.pid))
+      if (oldWorker) {
+        args.verbose && console.log(colors.yellow('Cluster: Stopping worker process ' + oldWorker.process.pid))
+        oldWorker.send('stop')
+      }
+    }
+  })
   w.send(args)
   return w
 }
 
 if (cluster.isMaster) {
+  cluster.setupMaster({
+    stdio: ['ignore', 'pipe', 'pipe', 'ipc']
+  })
+
   args = require('commander')
     .command('startserver')
     .version(pjson.version)
@@ -49,7 +123,13 @@ if (cluster.isMaster) {
   }
 
   cluster.on('exit', (worker, code, signal) => {
-    console.error(colors.red('Cluster: Worker process ' + worker.process.pid + ' died'))
+    let extra = ((code || '') + ' ' + (signal || '')).trim()
+    console.error(colors.red('Cluster: Worker process ' + worker.process.pid + ' exited ' + (extra ? '(' + extra + ')' : '')))
+    let index = workers.indexOf(worker)
+    if (index !== -1) workers.splice(index, 1)
+    if (code === 0) {
+      return
+    }
     setTimeout(() => {
       spawnWorker()
     }, 10 * 1000)
@@ -73,5 +153,17 @@ if (cluster.isMaster) {
     server.startServer(args).catch((err) => {
       console.error('' + err, err.stack || '')
     })
+
+    process.on('message', (message) => {
+      if (message === 'stop') {
+        console.log('Recieved stop signal')
+        const knex = require('./db').knex
+        knex.destroy(() => {
+          process.exit(0)
+        })
+      }
+    })
+
+    process.send('started')
   })
 }
