de4fd7eac7f05bbd028e84d0ac4fb130bd49513f
daniel-j <daniel.j.hede@gmail.com>
2018-05-28 11:31:23 +0200

    Lower highest AAC HLS quality to 160k

--- liq/scripts/streams.sh
+++ liq/scripts/streams.sh
@@ -8,10 +8,10 @@ streams_dir=`node -e "console.log(require('./scripts/config').server.streams_dir
 mkdir -p "$streams_dir/hls/radio"
 rm -f "$streams_dir/hls/radio/"*
 
-ffmpeg -hide_banner \
+ffmpeg -hide_banner -loglevel warning \
 -f s16le -ar 44100 -ac 2 -channel_layout stereo -i - \
 \
--c:a libfdk_aac -profile aac_main -afterburner 1 -b:a 192k -cutoff 18000 -movflags +faststart \
+-c:a libfdk_aac -profile aac_main -afterburner 1 -b:a 160k -cutoff 18000 -movflags +faststart \
 -metadata service_provider="Parasprite Radio" -f hls -hls_time 2 -hls_list_size 20 -hls_wrap 100 -hls_flags delete_segments "$streams_dir/hls/radio/high.m3u8" \
 \
 -c:a libfdk_aac -profile aac_he_v2 -afterburner 1 -b:a 64k -movflags +faststart \
