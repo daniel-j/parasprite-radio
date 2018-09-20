e7b4ca84e44de84227a29c830eb72bc5a7bab234
daniel-j <daniel.j.hede@gmail.com>
2018-09-20 12:41:34 +0200

    fix hls output, it was freezing

--- liq/scripts/streams.sh
+++ liq/scripts/streams.sh
@@ -8,7 +8,8 @@ streams_dir=`node -e "console.log(require('./scripts/config').server.streams_dir
 mkdir -p "$streams_dir/hls/radio"
 rm -f "$streams_dir/hls/radio/"*
 
-ffmpeg -hide_banner -loglevel warning -i - \
+ffmpeg -hide_banner -loglevel warning \
+-f s16le -ar 48000 -ac 2 -channel_layout stereo -i - \
 \
 -c:a libfdk_aac -profile aac_main -afterburner 1 -b:a 192k -cutoff 18000 -movflags +faststart \
 -metadata service_provider="Parasprite Radio" -f hls -hls_time 2 -hls_list_size 10 -hls_wrap 100 -hls_flags delete_segments+program_date_time "$streams_dir/hls/radio/high.m3u8" \
